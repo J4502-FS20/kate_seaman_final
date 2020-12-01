9ce6d3c404a29068ec76f358096763e2bdba7d9e
Kate <70589181+klsykn@users.noreply.github.com>
2020-12-01 13:09:29 -0600

    Update file.js

--- file.js
+++ file.js
@@ -35,12 +35,10 @@ function showSlides(n) {
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
-{slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;}
 }
-
 
   var img = document.getElementById("myImg");
   var modalImg = document.getElementById("img01");
