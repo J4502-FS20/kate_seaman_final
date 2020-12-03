ea39a787b3a35317d572870b15352afd6ab82ee7
klsykn <klsykn@umsystem.edu>
2020-12-03 11:57:20 -0600

    Fixed Modal

--- file.js
+++ file.js
@@ -38,3 +38,17 @@ slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;}
 }
+
+var modal = document.getElementById("myModal");
+  var img = document.getElementById("myImg");
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+  img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+    var span = document.getElementsByClassName("close")[0];
+      span.onclick = function() {
+      modal.style.display = "none";
+    }
